ec311484e122417611acd018f8396ffa2cba0def
Raul Arrabales <raul.arrabales@gmail.com>
2016-10-25 12:04:19 +0200

    Update SmallShinyApp.R

--- shiny/SmallShinyApp.R
+++ shiny/SmallShinyApp.R
@@ -1,10 +1,19 @@
 library(shiny)
 
 ui <- fluidPage(
-        "Hola Shiny",
-        sliderInput(inputId = "num", label = "Elije un número",
-                    value = 5, min = 1, max = 10))
+        "Hi Shiny!",
+        sliderInput(inputId = "num", label = "Choose a number",
+                    value = 5, min = 1, max = 1000),
+        plotOutput(outputId = "hist")
+      )
 
-server <- function(input, output){}
+server <- function(input, output)
+{
+  output$hist <- renderPlot({
+    title <- paste("Normal distribution of", input$num, "values")
+    hist(rnorm(input$num), main = title)
+  })
+  
+}
 
 shinyApp(ui = ui, server = server)
